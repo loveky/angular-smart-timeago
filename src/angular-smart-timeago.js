e696e4a4bdbafcee2cd1db3cddae878f2c05b952
loveky <ylzcylx@gmail.com>
2014-05-29 11:48:56 +0800

    add config constant

--- src/angular-smart-timeago.js
+++ src/angular-smart-timeago.js
@@ -1,17 +1,36 @@
 var ngSmartTimeago = angular.module("ngSmartTimeago", []);
 
-ngSmartTimeago.directive("timeago", [function() {
-  return {
-    restrict: "E",
-    scope: {
-      dir: "@",
-      suffix: "@"
-    },
-    link: function(scope, element, attrs) {
-      element.timeago({selector: "timeago"});
-      scope.$on('$destroy', function() {
-        element.data("timeago").stopTimer();
-      });
-    }
-  };
-}]);
+ngSmartTimeago.constant("ngSmartTimeagoConfig", {
+  attr: "datetime"
+});
+
+ngSmartTimeago.directive("timeago", ["ngSmartTimeagoConfig",
+  function(ngSmartTimeagoConfig) {
+    return {
+      restrict: "E",
+      scope: {
+        attr: "@"
+      },
+      link: function(scope, element, attrs) {
+        var scope_options = {};
+        if(angular.isDefined(scope.attr)) {
+          scope_options["attr"] = scope.attr;
+        }
+
+        var options = angular.extend(
+          {},
+          ngSmartTimeagoConfig,
+          scope_options,
+          {selector: "timeago"}
+        );
+        console.log(options);
+        element.timeago(options);
+
+        // Clear timer before destroying scope
+        scope.$on('$destroy', function() {
+          element.data("timeago").stopTimer();
+        });
+      }
+    };
+  }
+]);
